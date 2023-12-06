6ab09ff0b4915689bd61c9bd56bfa6b77f46f517
up shell <40010260+anggi135@users.noreply.github.com>
2023-12-07 04:12:27 +0700

    Update murotal.py

--- murotal.py
+++ murotal.py
@@ -1,7 +1,5 @@
 import requests
 import os
-
-# Fungsi untuk mendapatkan semua ayat dari Al-Quran untuk nomor surah dan edisi tertentu
 def get_surah_by_number(surah_number, edition):
     url = f'http://api.alquran.cloud/v1/surah/{surah_number}/{edition}'
     response = requests.get(url)
@@ -11,12 +9,8 @@ def get_surah_by_number(surah_number, edition):
     else:
         print("Failed to fetch surah")
         return None
-
-# Fungsi untuk memainkan audio menggunakan mpv di Termux
 def play_audio(url):
     os.system(f"mpv {url}")
-
-# Memasukkan nomor surah dan edisi Al-Quran
 surah_number = input("Masukkan nomor surah: ")
 edition = input("Masukkan edisi Al-Quran (contoh: en.asad): ")
 
